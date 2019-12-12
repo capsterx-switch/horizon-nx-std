stanuwu/libnx-rs-std

Fixed the std::os::raw c_char type.

src/libstd/os/raw/mod.rs
@@ -31,6 +31,7 @@
                                         target_arch = "arm",
                                         target_arch = "powerpc")),
           all(target_os = "openbsd", target_arch = "aarch64"),
+          all(target_os = "horizon", target_arch = "aarch64"),
           all(target_os = "fuchsia", target_arch = "aarch64")))]
 #[stable(feature = "raw_os", since = "1.1.0")] pub type c_char = u8;
 #[doc(include = "os/raw/char.md")]
@@ -46,6 +47,7 @@
                                             target_arch = "arm",
                                             target_arch = "powerpc")),
               all(target_os = "openbsd", target_arch = "aarch64"),
+              all(target_os = "horizon", target_arch = "aarch64"),
               all(target_os = "fuchsia", target_arch = "aarch64"))))]
 #[stable(feature = "raw_os", since = "1.1.0")] pub type c_char = i8;
 #[doc(include = "os/raw/schar.md")]

capsterx-switch/horizon-nx-std

src/libstd/os/raw/mod.rs
@@ -88,7 +88,8 @@ pub type c_char = u8;
             target_arch = "powerpc"
         )
     ),
-    all(target_os = "fuchsia", target_arch = "aarch64")
+    all(target_os = "fuchsia", target_arch = "aarch64"),
+    all(target_os = "horizon", target_arch = "aarch64")
 )))]
 #[stable(feature = "raw_os", since = "1.1.0")]
 pub type c_char = i8;